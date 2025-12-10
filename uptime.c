ffcaae5685763a50a2ff117f1984559566269889
Abish Mangal S B <abishmangal@gmail.com>
2025-12-10 18:14:43 +0530

    IITM Labs Completed

--- uptime.c
+++ uptime.c
@@ -5,15 +5,17 @@
 int main(int argc, char* argv[])
 {
 
-    if(argc!= 0)
+    if(argc!= 1)
     {
 
-        printf(2,"usage: uptime");
+        printf(2,"Usage: uptime\n");
         exit();
 
     }
 
-    printf("Uptime : %d",uptime());
+    printf(1,"Uptime : %d\n",uptime());
     exit();
 
 }
+
+// the gist of this lab is to find useful system calls from user.h and apply them to make cli commands
